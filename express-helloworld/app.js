411e4e40b89fcc90fd866a1722b9323396a24ed2
Student User <student@workstation.lab.example.com>
2022-02-17 14:13:19 -0500

    update version 3

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,7 +1,11 @@
-var response;
-applicationCache.get('/', function (req, res) {
-      response = 'This is version 2 of the app.' + '\n';
-          //send the response to the client//
-              res.send(response);
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
